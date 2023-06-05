c30c109b76e00997684d0b2ea445af022f99b419
User <User@WIN-8PIN49GOFPS>
2023-06-06 00:00:30 +0300

    Добавьте файлы проекта.

---
+++ Gudkov 3lab 1semstr/Gudkov 3lab 1semstr.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    system("chcp 1251");
+    string input;
+    cout << "Введите текст: ";
+    getline(cin, input);
+
+    int spaces = 0, tabs = 0;
+    for (int i = 0; i < input.length(); i++) {
+        if (input[i] == ' ') {
+            spaces++;
+        }
+        else if (input[i] == '\t') {
+            tabs++;
+        }
+        else {
+            cout << input[i];
+        }
+
+    }
+
+
+    cout << "\nУдалено пробелов: " << spaces << "\nУдалено табов: " << tabs;
+
+    return 0;
+}
